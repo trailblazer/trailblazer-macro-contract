2d99eb87bdebe16f73477a1f57ed33073d534f31
Marc Tich <m.tichelaar@gmail.com>
2018-03-08 16:27:38 +0100

    Adjusted to not use extensions

--- test/operation/dsl/contract_test.rb
+++ test/operation/dsl/contract_test.rb
@@ -7,6 +7,10 @@ require "test_helper"
 
 class DslContractTest < MiniTest::Spec
   module Call
+    class MyContract
+
+    end
+
     def call(params)
       validate(params, model: model=OpenStruct.new) { contract.sync }
       model
@@ -14,7 +18,7 @@ class DslContractTest < MiniTest::Spec
 
     def self.included(includer)
       includer.step Trailblazer::Operation::Model( OpenStruct, :new )
-      includer.step Trailblazer::Operation::Contract::Build()
+      includer.step Trailblazer::Operation::Contract::Build(constant: MyContract)
       includer.step Trailblazer::Operation::Contract::Validate()
       includer.step Trailblazer::Operation::Contract::Persist( method: :sync )
       # includer.> ->(op, *) { op["x"] = [] }
@@ -25,7 +29,6 @@ class DslContractTest < MiniTest::Spec
   # Operation::["contract.default.class"]
   # Operation::["contract.default.class"]=
   class Create < Trailblazer::Operation
-    include Contract
     self["contract.default.class"] = String
   end
 
@@ -42,31 +45,32 @@ class DslContractTest < MiniTest::Spec
   # ---
   # Op::contract Constant
   class Update < Trailblazer::Operation
+    include Call
+
     class IdContract < Reform::Form
       property :id
     end
 
-    extend Contract::DSL
-    contract IdContract
-
-    include Call
+    step Trailblazer::Operation::Contract::Build(constant: IdContract), replace: "contract.build"
   end
 
   # UT: subclasses contract.
-  it { Update["contract.default.class"].superclass.must_equal Update::IdContract }
+  it { Update.(params: {})["contract.default"].class.must_equal Update::IdContract }
   # IT: only knows `id`.
   it { Update.(params: {id: 1, title: "Coaster"})[:model].inspect.must_equal %{#<OpenStruct id=1>} }
 
   # Op::contract with inheritance
   # no ::contract call.
   class Upgrade < Update
+    class UpgradeContract < Reform::Form
+    end
+    step Trailblazer::Operation::Contract::Build(constant: UpgradeContract), replace: "contract.build"
   end
 
-  # UT: subclasses contract but doesn't share with parent.
-  it { Upgrade["contract.default.class"].superclass.must_equal Update::IdContract }
-  it { Upgrade["contract.default.class"].wont_equal Update["contract.default.class"] }
-  # IT: only knows `id`.
-  it { Upgrade.(params: {id: 1, title: "Coaster"})[:model].inspect.must_equal %{#<OpenStruct id=1>} }
+  # UT: replaces contract and doesn't share with parent.
+  it { Upgrade.(params: {})["contract.default"].class.must_equal Upgrade::UpgradeContract }
+  # IT: Doesn't know `id`.
+  it { Upgrade.(params: {id: 1, title: "Coaster"})[:model].inspect.must_equal %{#<OpenStruct>} }
 
   # ::contract B overrides old A contract.
   # this makes sure when calling contract(Constant), the old class gets wiped and is replaced with the new constant.
@@ -75,24 +79,26 @@ class DslContractTest < MiniTest::Spec
       property :title
     end
 
-    contract TitleContract
+    step Trailblazer::Operation::Contract::Build(constant: TitleContract), replace: "contract.build"
   end
 
   # UT: subclasses contract.
-  it { Upsert["contract.default.class"].superclass.must_equal Upsert::TitleContract }
+  it { Upsert.(params: {})["contract.default"].class.must_equal Upsert::TitleContract }
   # IT: only knows `title`.
   it { Upsert.(params: {id: 1, title: "Coaster"})[:model].inspect.must_equal %{#<OpenStruct title="Coaster">} }
 
   # ::contract B do ..end overrides and extends new.
   # using a constant will wipe out the existing class.
   class Upside < Update
-    contract Upsert::TitleContract do
+    class Upsert::TitleContract
       property :id
     end
+
+    step Trailblazer::Operation::Contract::Build(constant: Upsert::TitleContract), replace: "contract.build"
   end
 
   # UT: subclasses contract.
-  it { Upside["contract.default.class"].superclass.must_equal Upsert::TitleContract }
+  it { Upside.(params: {})["contract.default"].class.must_equal Upsert::TitleContract }
   # IT: only knows `title`.
   it { Upside.(params: {id: 1, title: "Coaster"})[:model].inspect.must_equal %{#<OpenStruct title="Coaster", id=1>} }
 
@@ -101,37 +107,30 @@ class DslContractTest < MiniTest::Spec
   # (with block)
   class Delete < Trailblazer::Operation
     include Call
-    extend Contract::DSL
-    contract do
+
+    class TitleContract < Reform::Form
       property :title
     end
+
+    step Trailblazer::Operation::Contract::Build(constant: TitleContract), replace: "contract.build"
   end
 
-  # UT: contract path is "contract.default.class"
-  it { Delete["contract.default.class"].definitions.keys.must_equal ["title"] }
   # IT: knows `title`.
   it { Delete.(params: {id: 1, title: "Coaster"})[:model].inspect.must_equal %{#<OpenStruct title=\"Coaster\">} }
 
-  class Wipe < Trailblazer::Operation
-    extend Contract::DSL
-
-    self["x"] = contract do end
-  end
-  # UT: ::contract returns form class
-  it { Wipe["x"].superclass.must_equal Reform::Form }
-
   # subsequent calls merge.
   class Remove < Trailblazer::Operation
-    extend Contract::DSL
     include Call
 
-    contract do
+    class TitleContract < Reform::Form
       property :title
     end
 
-    contract do
+    class IdContract < TitleContract
       property :id
     end
+
+    step Trailblazer::Operation::Contract::Build(constant: IdContract), replace: "contract.build"
   end
 
   # IT: knows `title` and `id`, since contracts get merged.
@@ -151,17 +150,20 @@ class DslContractTest < MiniTest::Spec
   describe "inheritance across operations" do
     # inheritance
     class Operation < Trailblazer::Operation
-      extend Contract::DSL
-      contract do
+      class MyContract < Reform::Form
         property :title
         property :band
       end
 
+      step Model( OpenStruct, :new )
+      step Contract::Build(constant: MyContract)
+
       class JSON < self
-        contract do # inherit Contract
+        class JsonContract < MyContract
           property :genre, validates: {presence: true}
           property :band, virtual: true
         end
+        step Contract::Build(constant: JsonContract), replace: "contract.build"
       end
 
       class XML < self
@@ -169,24 +171,25 @@ class DslContractTest < MiniTest::Spec
     end
 
     # inherits subclassed Contract.
-    it { Operation["contract.default.class"].wont_equal Operation::JSON["contract.default.class"] }
-    it { Operation::XML["contract.default.class"].superclass.must_equal Reform::Form }
+    it { Operation.(params: {})["contract.default"].must_be_instance_of Operation::MyContract }
+    it { Operation::JSON.(params: {})["contract.default"].must_be_instance_of Operation::JSON::JsonContract }
+    it { Operation::XML.(params: {})["contract.default"].must_be_instance_of Operation::MyContract }
 
     it do
-      form = Operation["contract.default.class"].new(OpenStruct.new)
+      form = Operation.(params: {})["contract.default"]
       form.validate({})#.must_equal true
       form.errors.to_s.must_equal "{}"
 
-      form = Operation::JSON["contract.default.class"].new(OpenStruct.new)
+      form = Operation::JSON.(params: {})["contract.default"]
       form.validate({})#.must_equal true
       form.errors.to_s.must_equal "{:genre=>[\"can't be blank\"]}"
     end
 
     # allows overriding options
     it do
-      form = Operation::JSON["contract.default.class"].new(song = OpenStruct.new)
+      form = Operation::JSON.(params: {})["contract.default"]
       form.validate({genre: "Punkrock", band: "Osker"}).must_equal true
-      form.sync
+      song = form.sync
 
       song.genre.must_equal "Punkrock"
       song.band.must_be_nil
@@ -199,9 +202,8 @@ class DslContractTest < MiniTest::Spec
     end
 
     class OpWithExternalContract < Trailblazer::Operation
-      extend Contract::DSL
-      contract SongForm
       include Call
+      step Trailblazer::Operation::Contract::Build(constant: SongForm), replace: "contract.build"
     end
 
     it { OpWithExternalContract.(params: {"songTitle"=> "Monsterparty"})["contract.default"].songTitle.must_equal "Monsterparty" }
@@ -213,17 +215,22 @@ class DslContractTest < MiniTest::Spec
     end
 
     class OpNotExtendingContract < Trailblazer::Operation
-      extend Contract::DSL
-      contract DifferentSongForm
       include Call
+
+      class MyContract < DifferentSongForm
+      end
+
+      step Trailblazer::Operation::Contract::Build(constant: MyContract), replace: "contract.build"
     end
 
     class OpExtendingContract < Trailblazer::Operation
-      extend Contract::DSL
-      contract DifferentSongForm do
+      include Call
+
+      class SongContract < DifferentSongForm
         property :genre
       end
-      include Call
+
+      step Trailblazer::Operation::Contract::Build(constant: SongContract), replace: "contract.build"
     end
 
     # this operation copies DifferentSongForm and shouldn't have `genre`.
@@ -248,39 +255,45 @@ class DslContractTest < MiniTest::Spec
 
   describe "Op.contract :name, Form" do
     class Follow < Trailblazer::Operation
-      ParamsForm = Class.new
+      class ParamsForm < Reform::Form
+      end
 
-      extend Contract::DSL
-      contract :params, ParamsForm
+      step Trailblazer::Operation::Contract::Build(constant: ParamsForm)
     end
 
-    it { Follow["contract.params.class"].superclass.must_equal Follow::ParamsForm }
+    it { Follow.(params: {})["contract.default"].must_be_instance_of Follow::ParamsForm }
   end
 
   describe "Op.contract :name do..end" do
     class Unfollow < Trailblazer::Operation
-      extend Contract::DSL
-      contract :params do
+      class MyContract < Reform::Form
         property :title
       end
+
+      step Model( OpenStruct, :new )
+      step Trailblazer::Operation::Contract::Build(constant: MyContract, name: "params")
     end
 
-    it { Unfollow["contract.params.class"].superclass.must_equal Reform::Form }
+    it { Unfollow.call(params: {})["contract.params"].must_be_instance_of Unfollow::MyContract }
   end
 
   # multiple ::contract calls.
   describe "multiple ::contract calls" do
     class Star < Trailblazer::Operation
-      extend Contract::DSL
-      contract do
+      class MyContract < Reform::Form
         property :title
       end
 
-      contract do
+      class MySecondContract < Reform::Form
         property :id
       end
+
+      step Model( OpenStruct, :new )
+      step Contract::Build(constant: MyContract)
+      step Contract::Build(constant: MySecondContract, name: "second")
     end
 
-    it { Star["contract.default.class"].definitions.keys.must_equal %w[title id]  }
+    it { Star.(params: {})["contract.default"].must_be_instance_of Star::MyContract }
+    it { Star.(params: {})["contract.second"].must_be_instance_of Star::MySecondContract }
   end
 end
