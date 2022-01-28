91ed50efa892f891403b2c566b70e428c26f37ea
Nick Sutterer <apotonick@gmail.com>
2022-01-28 18:32:03 +0100

    instead of hard-coding `:inject`, use the new composable `Inject()`.

--- lib/trailblazer/macro/contract/validate.rb
+++ lib/trailblazer/macro/contract/validate.rb
@@ -20,8 +20,8 @@ module Trailblazer
 
         # Build a simple Railway {Activity} for the internal flow.
         activity = Class.new(Activity::Railway(name: "Contract::Validate")) do
-          step extract,  id: "#{params_path}_extract", Output(:failure) => End(:extract_failure), inject: [extract_injections] unless skip_extract# || representer
-          step validate, id: "contract.#{name}.call", inject: [validate_injections]
+          step extract,  id: "#{params_path}_extract", Output(:failure) => End(:extract_failure), Activity::Railway.Inject() => extract_injections unless skip_extract# || representer
+          step validate, id: "contract.#{name}.call", Activity::Railway.Inject() => validate_injections
         end
 
         options = activity.Subprocess(activity)
